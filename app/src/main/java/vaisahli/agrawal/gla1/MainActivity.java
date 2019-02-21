7d7227b23fd3c8131ba567d2093ce24c89795676
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-21 11:19:05 +0530

    object create and id

--- app/src/main/java/vaisahli/agrawal/gla1/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/gla1/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt1;
+    Button Bt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        bt1 = findViewById(R.id.Button_1);
+        Bt2 = findViewById(R.id.hello);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
